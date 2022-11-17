0e50015cfdecaf977aadab85fff62bee45f5c8ee
Kim Kyubin <skipperkim307@gmail.com>
2022-11-17 17:51:34 +0900

    시작

---
+++ src/main/java/com/finescore/moneycookie/MoneycookieApplication.java
@@ -0,0 +1,13 @@
+package com.finescore.moneycookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneycookieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneycookieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finescore/moneycookie/CookiemoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finescore.moneycookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiemoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
