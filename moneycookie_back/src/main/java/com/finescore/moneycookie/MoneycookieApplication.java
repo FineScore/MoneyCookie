9334e13974449fa691c40e0a573466a0e2fac4bd
Kim Kyubin <skipperkim307@gmail.com>
2022-12-20 22:06:39 +0900

    [Back] 폴더 구조 변경 및 Spring-Vue 연동

--- moneycookie_back/src/main/java/com/finescore/moneycookie/MoneycookieApplication.java
+++ moneycookie_back/src/main/java/com/finescore/moneycookie/MoneycookieApplication.java
@@ -2,8 +2,9 @@ package com.finescore.moneycookie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MoneycookieApplication {
 
 	public static void main(String[] args) {
--- moneycookie_back/vue-project/vue.config.js
+++ moneycookie_back/vue-project/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: "../src/main/resources/static",
+  devServer: {
+    proxy: "http://localhost:8080",
+  },
 });
