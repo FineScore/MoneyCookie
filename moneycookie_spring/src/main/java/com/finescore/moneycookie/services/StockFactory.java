f269df484d7072bf419a76d38d73544fd77c9cde
Kim Kyubin <skipperkim307@gmail.com>
2023-01-09 22:24:56 +0900

    [Back] totalPrice nowPrice 분리

--- moneycookie_spring/src/main/java/com/finescore/moneycookie/services/StockFactory.java
+++ moneycookie_spring/src/main/java/com/finescore/moneycookie/services/StockFactory.java
@@ -2,6 +2,7 @@ package com.finescore.moneycookie.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.finescore.moneycookie.models.Price;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -14,23 +15,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 @Service
+@AllArgsConstructor
 public class StockFactory {
-    public String getNowPrice(String ticker) throws ParserConfigurationException, IOException, SAXException {
+    private final ObjectMapper objectMapper;
+
+    public ArrayList<Price> getTotalPrice(String ticker) throws ParserConfigurationException, IOException, SAXException {
+        String priceUrl = "https://fchart.stock.naver.com/sise.nhn?timeframe=day&count=6000&requestType=0&symbol=%s";
+        String url = String.format(priceUrl, ticker);
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = builderFactory.newDocumentBuilder();
-        ObjectMapper mapper = new ObjectMapper();
         ArrayList<Price> priceList = new ArrayList<>();
-        String url = "https://fchart.stock.naver.com/sise.nhn?timeframe=day&count=6000&requestType=0&symbol=005930";
 
         Document document = builder.parse(url);
         document.getDocumentElement().normalize();
         NodeList list = document.getElementsByTagName("item");
+
         for (int i = 0; i < list.getLength(); i++) {
             String[] lists = list.item(i).getAttributes().getNamedItem("data").getNodeValue().split("\\|");
             Price price = new Price(lists[0], Integer.parseInt(lists[4]));
             priceList.add(price);
         }
 
-        return mapper.writeValueAsString(priceList);
+        return priceList;
+    }
+
+    public String getNowPrice(String ticker) throws ParserConfigurationException, IOException, SAXException {
+        ArrayList<Price> totalPrice = getTotalPrice(ticker);
+
+        return objectMapper.writeValueAsString(totalPrice.get(totalPrice.size() - 1));
     }
 }
